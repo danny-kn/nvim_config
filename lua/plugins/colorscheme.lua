02730c1b0dfc3b076cdc97951df5fdc738a3b50d
Danny Kan <danny_kn@proton.me>
2025-05-19 20:58:23 -0400

    dev/ added a catppuccin color scheme and a lazygit plugin.

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+      })
+      vim.cmd("colorscheme catppuccin-mocha")
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorsheme = "catppuccin-mocha",
+    },
+  },
+}
